4f92936ee3340109064b1347edf4d2e39fa34edc
Yash Kumar <yashkumar7999@gmail.com>
2026-05-06 10:08:06 +0530

    .NET solution+frontend setup

---
+++ TicketBooking.Core/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace TicketBooking.Core;
+
+public class Class1
+{
+
+}
---
+++ TicketBooking.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace TicketBooking.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
